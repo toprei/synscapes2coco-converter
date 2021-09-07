137507c97b38bd397f0d033b71f848338fcb4b3d
Preintner, Tobias Peter (EZRET) <tobias-peter.preintner@man.eu>
2021-09-07 19:54:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import json
+from polygon_converter import create_sub_masks
+from polygon_converter import create_sub_mask_annotation
+import numpy as np                                 # (pip install numpy)
+from skimage import measure                        # (pip install scikit-image)
+from shapely.geometry import Polygon, MultiPolygon # (pip install Shapely)
+from PIL import Image
+
+
+if __name__ == '__main__':
+    # list for annotations to dump
+    annotation_list = []
+    images_list = []
+    instance_id = 0
+    for i in range(24501, 25000+1):
+        print("processing image: "+str(i))
+        img = Image.open('/media/hdd1/Public_Datasets/Synscapes/synscapes/Synscapes/img/instance/'+str(i)+'.png')
+        with open('/media/hdd1/Public_Datasets/Synscapes/synscapes/Synscapes/meta/'+str(i)+'.json') as jsonFile:
+            jsonMeta = json.load(jsonFile)
+            jsonFile.close()
+
+        instance = jsonMeta['instance']
+        classes = instance['class']
+        occluded = instance['occluded']
+
+        rgb_list = []
+        for id, class_id in classes.items():
+            # create list to filter only instances of class 24 (person) and less than some occlusion threshold
+            if class_id == 24 and occluded[id] <= 0.50:
+                rgb_list.append((int(id) & 255,int(id) >> 8 & 255,int(id) >> 16))
+
+        # create submasks of image i
+        image_submasks = create_sub_masks(img, rgb_list)
+
+        # create for each submask a polygon annotation
+        for submask in image_submasks.values():
+            annotation = create_sub_mask_annotation(submask, i, 1, instance_id, 0)
+            if len(annotation["segmentation"]) != 0:
+                annotation_list.append(annotation)
+                instance_id = instance_id + 1
+
+        width, height = img.size
+        image = {"id": i, "width": width, "height": height, "file_name": str(i)+".png"}
+        images_list.append(image)
+
+    print("start creating json")
+    json_data = {
+        "images": images_list,
+        "categories": [{"id": 1, "name": "person"}],
+        "annotations": annotation_list
+    }
+    print("start dumping json")
+    with open('my_annotation.json', 'w') as jsonFile:
+        json.dump(json_data, jsonFile)
+        jsonFile.close()
+
+    print("Created successfully annotation for " + str(instance_id) + " instances")
+    print("done!")
+
+
+    # (R, G, B)
+    # (int(id) & 255, (int(id) >> 8) & 255),int(id) >> 16)
+    # #rgb = '('+ str(int(id) & 255) + ',' + str((int(id) >> 8) & 255) + ',' + str(int(id) >> 16) +')'
+
+
+
+
+
+
+
+
+
---
+++ polygon_converter.py
@@ -0,0 +1,100 @@
+import numpy as np                                 # (pip install numpy)
+from skimage import measure                        # (pip install scikit-image)
+from shapely.geometry import Polygon, MultiPolygon # (pip install Shapely)
+from PIL import Image
+
+# R + G * 256 + B * 256^2.
+# sub_mask , image_id, category_id, annotation_id, is_crowd == False
+def create_sub_mask_annotation(sub_mask, image_id, category_id, annotation_id, is_crowd):
+    # Find contours (boundary lines) around each sub-mask
+    # Note: there could be multiple contours if the object
+    # is partially occluded. (E.g. an elephant behind a tree)
+    sub_mask = np.asanyarray(sub_mask)
+    contours = measure.find_contours(sub_mask, 0.5, positive_orientation='low')
+    segmentations = []
+    polygons = []
+    for contour in contours:
+        # Flip from (row, col) representation to (x, y)
+        # and subtract the padding pixel
+        for i in range(len(contour)):
+
+            row, col = contour[i]
+            contour[i] = (col - 1, row - 1)
+        # Make a polygon and simplify it
+        poly = Polygon(contour)
+        poly = poly.simplify(1.0, preserve_topology=False)
+        if poly.geom_type is not 'MultiPolygon':
+            polygons.append(poly)
+            try:
+                segmentation = np.array(poly.exterior.coords).ravel().tolist()
+                #segmentation.clip(0)
+                segmentations.append(segmentation)
+            except:
+                print(poly.geom_type, len(poly.geoms), poly)
+        else:
+            for i in range(0, len(poly.geoms)):
+                segmentation = np.array(poly[i].exterior.coords).ravel().tolist()
+                #segmentation.clip(0)
+                segmentations.append(segmentation)
+
+    # Combine the polygons to calculate the bounding box and area
+    multi_poly = MultiPolygon(polygons)
+    if not multi_poly.is_empty:
+        x, y, max_x, max_y = multi_poly.bounds
+        width = max_x - x
+        height = max_y - y
+        bbox = (x, y, width, height)
+        area = multi_poly.area
+        # size = [width - x + 1, height - y + 1]
+        # segmentations["size"].append(size)
+    else:
+        bbox = (0, 0, 0, 0)
+        area = 0
+        # size = [0, 0]
+        # segmentations["size"].append(size)
+
+    # round segmentation int numbers and remove empty lists
+    rounded_segmentations = []
+    for num_list in segmentations:
+        if len(num_list) != 0:
+            rounded_segmentations.append([round(x) for x in num_list])
+
+    annotation = {
+        'id': annotation_id,
+        'image_id': image_id,
+        'segmentation': rounded_segmentations,
+        'category_id': category_id,
+        'iscrowd': is_crowd,
+        'area': round(area),
+        'bbox': [round(num) for num in bbox]
+    }
+    return annotation
+
+
+
+def create_sub_masks(mask_image, rgb_list):
+    width, height = mask_image.size
+    # Initialize a dictionary of sub-masks indexed by RGB colors
+    sub_masks = {}
+    counter = 0
+    for x in range(width):
+        for y in range(height):
+            # Get the RGB values of the pixel
+            pixel = mask_image.getpixel((x,y))[:3]
+            # If the pixel is in the rgb list
+            if pixel in rgb_list:
+                # Check to see if we've created a sub-mask...
+                pixel_str = str(pixel)
+                sub_mask = sub_masks.get(pixel_str)
+                if sub_mask is None:
+                    # Create a sub-mask (one bit per pixel) and add to the dictionary
+                    # Note: we add 1 pixel of padding in each direction
+                    # because the contours module doesn't handle cases
+                    # where pixels bleed to the edge of the image
+                    counter = counter + 1
+                    sub_masks[pixel_str] = Image.new('1', (width+2, height+2))
+                # Set the pixel value to 1 (default is 0), accounting for padding
+                sub_masks[pixel_str].putpixel((x+1, y+1), 1)
+
+    # print(counter)
+    return sub_masks
